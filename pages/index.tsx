3029954d13a093dd08a45d00334f995be6db5b49
xyntechx <xyntechx@gmail.com>
2022-03-05 17:16:20 +0800

    📇 OG image test

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ const Home: NextPage = () => {
                 />
                 <meta
                     property="og:image"
-                    content="https://tedxri.com/logo-square.jpeg"
+                    content="https://tedxri.com/logo.png"
                 />
                 <meta property="og:url" content="https://tedxri.com/" />
                 <meta property="og:type" content="website" />
