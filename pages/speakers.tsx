74881c29c4bb834e83b0933393ab0efad9ed062e
xyntechx <xyntechx@gmail.com>
2022-06-19 21:21:34 +0800

    ⚡ Update speaker quote and bio

--- pages/speakers.tsx
+++ pages/speakers.tsx
@@ -329,13 +329,13 @@ const EricLi = ({ closePopUp, setShow }: SpeakerArgs) => {
                 <p>
                     Eric Li is a 19-year-old founder, programmer, and innovator.
                     He holds two patents and is the founder of TEDxNewton,
-                    Singapore's nationwide conference on science and technology,
-                    attracting over 6 million impressions/views from people in
-                    150+ countries and all 7 continents. A crypto native, Eric
-                    has held cryptocurrency since 2017 and is passionate about
-                    decentralised finance (DeFi), especially the technology
-                    underpinning automated market makers and decentralised
-                    lending & borrowing.
+                    Singapore&apos;s nationwide conference on science and
+                    technology, attracting over 6 million impressions/views from
+                    people in 150+ countries and all 7 continents. A crypto
+                    native, Eric has held cryptocurrency since 2017 and is
+                    passionate about decentralised finance (DeFi), especially
+                    the technology underpinning automated market makers and
+                    decentralised lending & borrowing.
                 </p>
                 <span className={popup.images}>
                     <Link href="https://linkedin.com/in/singapore/">
